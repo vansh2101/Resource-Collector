619354f944b437e5ee23b6843ca7d1d2b8d1d9b2
Vansh Sachdeva <vansh2101sachdeva@gmail.com>
2022-07-31 23:41:55 +0530

    initial commit

--- ResourceCollector/settings.py
+++ ResourceCollector/settings.py
@@ -31,7 +31,10 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'https://resourcecollector.herokuapp.com',
+    'localhost'
+]
 
 
 # Application definition
